3ab9fbb1e4e1f12e73d24dc3e6d0705de2419d3d
Abdiaziz Haredh <abdiazizhared64@gmail.com>
2026-04-29 12:44:52 +0300

    initial commit

---
+++ functions.py
@@ -0,0 +1,38 @@
+def my_function():
+    return "this is function"
+
+message = my_function()
+print(message)
+
+student1_name = "abdiaziz"
+student2_name = "AHMED"
+student3_name = "halima"
+#f-string
+message = f"hello {student1_name}  welcome to the clASS"
+message = f"hello {student2_name}  welcome to the clASS"
+message = f"hello {student3_name}  welcome to the clASS"
+print(message)
+
+def greet_student(name ,email="user@gmail.com"):
+    message = f"hello {name}  welcome to the clASS and your email is {email}"
+
+    print(message)
+
+greet_student("mohamed" )
+greet_student("zamzam" , "zamzam@gmail.com")
+greet_student("ali")
+
+email = "student@gau.ac.ke"
+
+def list_students(*args, **kwargs):
+    print(kwargs)
+    global email
+    email = "halima@gmail.com"
+    print(email)
+    return f"the first student is {args[0]} and the second is  {args[1]} "
+    
+
+students = list_students("john" ,"doe" ,"musa" ,"fdghdh" , age = 23 , gender = "male")
+print(students)
+
+
---
+++ index.py
@@ -0,0 +1,71 @@
+
+
+print("hello world" , "gaggggg" , sep="/" , end=" ")
+print("python is easy" , "gaggggg" , sep="/", end=" ")
+print("am at garissa university" , "gaggggg" , sep="/", end=" ")
+
+file = open("file.txt" , "w")
+
+print("i'm learning python" , file= file)
+
+
+
+
+payment_1 = "abdi"
+print(1)
+
+programming_language = "python"
+programming_language = "java"
+print(programming_language)
+
+invoice_number  = 3
+print(invoice_number)
+
+a,b,c = 1,2,3
+print(a,b,c ,sep="\n")
+
+number = 2.5
+print(type(number))
+
+name = "abdi"
+print(type(name))
+print(name.islower())
+
+is_online = True
+print(is_online)
+print(type(is_online))
+
+fruits = ["apple" , "banana" , "orange"] 
+fruits[0] = "watermelon"
+
+print(fruits[0])
+
+#tuples
+
+students = ("abdi" , "halima" , "abdiaziz")
+
+print(students[0])
+
+
+#sets
+
+emails = {"abdi@gmail.com" , "halima@gmail.com" , "halima@gmail.com" ,"abdiaziz@gmail.com"}
+
+print(emails[0])
+
+#dictionaries
+
+book = {
+   "title" : "resiliant of echoes",
+   "author" : "abdiaziz",
+   "Year_published" :2002
+}
+
+print(book["authorr"])
+
+
+
+
+
+
+ 
