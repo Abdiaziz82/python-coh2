c0f15a85f3a0ad1c6e1403b57831b389ecf8c5d7
Abdiaziz Haredh <abdiazizhared64@gmail.com>
2026-06-11 23:04:03 +0300

    update

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request ,render_template ,jsonify
 from model import db ,Student
-
+from flask_cors import CORS
 app = Flask(__name__)
+
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///university.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+  
 
 @app.route('/students' ,methods= ["GET"])
 def get_students():
@@ -57,7 +60,7 @@ def delete_student(id):
     if not student:
         return jsonify({"error": "student not found"})
     
-    db.session.delete(student)
+    
     db.session.commit()
     return jsonify({"message": "student deleted"}) , 200
 
